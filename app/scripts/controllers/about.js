015556447fcc271e04f7f83b2bca75a5621cdc67
Your Name <you@example.com>
2017-12-06 23:07:21 -0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name intltelinputApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the intltelinputApp
+ */
+angular.module('intltelinputApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name intltelinputApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the intltelinputApp
+ */
+angular.module('intltelinputApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
